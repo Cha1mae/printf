c2705adf7e5479e37e9f7f5254c12af57a7db91c
Cha1mae <chaimastaifa@gmail.com>
2023-08-20 14:34:31 -0700

    testing

--- main.h
+++ main.h
@@ -5,6 +5,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <limits.h>
+#define F_MINUS 1
+#define F_PLUS  2
+#define F_ZERO  4
+#define F_HASH  8
+#define F_SPACE 16
 
 int _putchar(char character);
 int printf_char(va_list args, int printed);
