2e34e00bf5bb6c03492200ab5b50ac1c8f33d83d
Cha1mae <chaimastaifa@gmail.com>
2023-08-21 15:28:06 -0700

    testing

--- test_cases.c
+++ test_cases.c
@@ -4,48 +4,31 @@ int main(void)
 {
 int printed;
 char *address;
-
     /*Character*/
 printf("Character:[%c]\n", 'H');
 printed = _printf("Character:[%c]\n", 'H');
-
     /*String*/
 printf("String:[%s]\n", "I am a string !");
 printed += _printf("String:[%s]\n", "I am a string !");
-
     /*Integer*/
 printf("Negative:[%d]\n", -762534);
 printed += _printf("Negative:[%d]\n", -762534);
-
     /*Unsigned Integer*/
 printf("Unsigned:[%u]\n", 2147484671);
 printed += _printf("Unsigned:[%u]\n", 2147484671);
-
-    /*Octal*/
-printf("Unsigned octal:[%o]\n", 20000001777);
+printf("Unsigned octal:[%o]\n", 20000001777);/*Octal*/
 printed += _printf("Unsigned octal:[%o]\n", 20000001777);
-
-    /*Hexadecimal (lowercase and uppercase)*/
-printf("Unsigned hexadecimal:[%x, %X]\n", 2147484671, 2147484671);
+printf("Unsigned hexadecimal:[%x, %X]\n", 2147484671, 2147484671);/*Hex*/
 printed += _printf("Unsigned hexadecimal:[%x, %X]\n", 2147484671, 2147484671);
-
-    /*Address*/
-address = "0x7ffe637541f0";
+address = "0x7ffe637541f0";/*Address*/
 printf("Address:[%p]\n", (void *)address);
 printed += _printf("Address:[%p]\n", (void *)address);
-
-    /*Percent*/
-printf("Percent:[%%]\n");
+printf("Percent:[%%]\n");/*Percent*/
 printed += _printf("Percent:[%%]\n");
-
-    /*Length modifier*/
-printf("Len:[%12d]\n", 123456789);
+printf("Len:[%12d]\n", 123456789);/*Length modifier*/
 printed += _printf("Len:[%12d]\n", 123456789);
-
-    /*Unknown format specifier*/
-printf("Unknown:[%r]\n");
+printf("Unknown:[%r]\n");/*Unknown format specifier*/
 printed += _printf("Unknown:[%r]\n");
 
 return (0);
 }
-
