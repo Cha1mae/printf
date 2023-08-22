7b33a59bdfdeb194fd2273d582c0b88fb1b2631d
Maedot <124568412+maedotamha@users.noreply.github.com>
2023-08-22 20:43:16 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,1 +1,29 @@
 //test
+#include "main.h"
+
+/**
+ * printf_reverse - Prints a string in reverse order
+ * @args: List of arguments (string)
+ * @printed: Number of characters printed so far
+ * Return: Updated count of printed characters
+ */
+
+int printf_reverse(va_list args, int printed)
+{
+	char *str = va_arg(args, char *);
+	int len = 0, i;
+
+	while (str[len])
+	{
+		len++;
+	}
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		printed++;
+
+	}
+
+	return (printed);
+}
